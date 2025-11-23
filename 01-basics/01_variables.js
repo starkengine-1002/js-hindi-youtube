76448ca0459df7fa2902b2f997587119dc9f68eb
ishhhh <aishwaryacherupally5@gmail.com>
2025-11-23 10:11:04 +0000

    datatypes

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId =144553
+let accountEmail = "aishwarycherupally1@gmail.com"
+var accountPassword ="12345"
+accountCity ="jaipur"
+// accountId = 2 not allowed
+// so here constant declare means only one way but while declaring variable we can use let and var and var is not that used because of scope and scope means { } and issues in block scope and functional scope used in for and if etc sooooooooo so use const and let 
+/*
+*/
+accountEmail ="kju@nic.com"
+accountCity ="banglore"
+let accountState ;
+// use ; or not your wish
+// console.log(accountId);
+console.log([accountId , accountEmail, accountPassword, accountCity]);
+
+// console.log(accountEmail); it is kind of old so use table to print all like see down
+console.table([accountId , accountEmail, accountPassword, accountCity , accountState]);
+
+
---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+// alert (3+3)  // we are using nodejs ,not browser ans serach alert documentation in mdn or in ecma 
+console.log(3+3) ;console.log("ish")
+console.log(3+3) 
+console.log("ish")
+
+let name ="ish"
+let age = 18 
+let isLoggedIn = true
+let state; 
+
+// number =>2 to power of 52 or 53
+//bigint
+// string => "" or ''
+//boolean => true/false
+//null is a standalone value and it is representation of empty value 
+//undefined => variable is written but not defined value
+//symbol => to find uniqueness 
+
+//object
+
+console.log(typeof null) // output is object
+console.log(typeof undefined ) // output  is undefined 
